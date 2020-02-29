388697e50f65e0676a299160fc74e075d829001a
peidrao <contatopedrorn@gmail.com>
2020-02-29 19:31:32 +0000

    save client to file

--- funcoes.cpp
+++ funcoes.cpp
@@ -30,6 +30,14 @@ double Cliente::get_saldo() const {
 void Cliente::mensagem() const {
   cout << conta_ << setw(6) << "|" << nome_ << setw(6) << "| R$" << saldo_ << endl;    
 }
+void salvar_conta() {
+  Cliente cliente_;
+  ofstream arquivo;
+  arquivo.open("contas.dat", ios::binary | ios::app);
+  cliente_.criar_conta();
+  arquivo.write(reinterpret_cast<char *>(&cliente_), sizeof(Cliente));
+  arquivo.close();
+}
 
 void Cliente::criar_conta(){
   char opcao;
@@ -41,8 +49,7 @@ void Cliente::criar_conta(){
   conta_ = senha_ + (rand() % 1000);
   cout << "Conta: " << conta_ << endl;
 
-  do
-  {
+  
     cout << "+=============================+" << endl
        << "| DESEJA FAZER ALGUM DEPÓSITO |" << endl
        << "| | ( S ) SIM ||| ( N ) NÃO | |" << endl
@@ -50,15 +57,68 @@ void Cliente::criar_conta(){
        << "|DIGITE UMA OPCAO >>>>> " ;
   
   cin >> opcao;
-  if (opcao == 's' || 'S')
+  if (opcao == 's' || opcao ==  'S')
   {
     cout << "QUANTO DESEJA DEPOSITAR: R$ ";
     cin >> saldo_;
   } else {
     cout << "Opção inválida! Digite novamente!" << endl;
   }
-  
-  } while (opcao != 'S' || 's');
+
   cout << "Conta criada com sucesso!" << endl;
   
+}
+
+void menu_cliente() {
+  char opcao;
+  int contaOrigem, contaDestino;
+  double valorDepositado, valorRetirado;
+
+  cout << endl
+       << "|----------------------|" << endl
+       << "|-- MENU DO CLIENTE -- |" << endl
+       << "|[ 1 ] - Depositar ....|" << endl
+       << "|[ 2 ] - Sacar ........|" << endl
+       << "|[ 3 ] - Transferir ...|" << endl
+       << "|[ 4 ] - Ver extrato ..|" << endl 
+       << "|[ 0 ] - Sair .........|" << endl
+       << "|----------------------|" << endl
+       << "|> ";
+  
+  cin >> opcao;
+  switch (opcao)
+  {
+  case '1':
+    cout << "Depóstio!" << endl;
+    break;
+  
+  default:
+    break;
+  }
+}
+
+char opcao_menu() {
+  char opcao;
+  system("clear");
+  cout << "+==============================+" << endl
+       << "| JÁ POSSUI CONTA NESSE BANCO  |" << endl
+       << "| | ( S ) SIM ||| ( N ) NÃO |  |" << endl
+       << "+=============================+" << endl
+       << "|> " ;
+  
+  do
+  {
+    cin >> opcao;
+    if (opcao == 's' || opcao == 'S')
+  {
+    cout << "esperando funcao!" << endl;
+  } else if (opcao == 'n' || opcao == 'N')
+  {
+    salvar_conta();
+  } else {
+    cout << "Digite uma opção válida!" << endl;
+  }
+  } while (opcao != 's' || 'S' || 'n' || 'N');
+  
+  return opcao;
 }
